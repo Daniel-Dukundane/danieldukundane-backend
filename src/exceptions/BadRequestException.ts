a65609690ff2b72e7bb3c6c2226d984f371b75d1
Daniel Dukundane <52023375+Daniel-Dukundane@users.noreply.github.com>
2022-02-17 17:25:22 +0200

    First commit

--- src/exceptions/BadRequestException.ts
+++ src/exceptions/BadRequestException.ts
@@ -1,3 +1,7 @@
+/**
+ * @author: Daniel Dukundane
+ * @description: meter not found exception handler implementation
+ */
 import { HttpException, HttpStatus } from '@nestjs/common';
 
 export class CustomizedBadRequestException extends HttpException {
